b0475b6b3677c4b4bcbcd61d8379023253f658de
Jean Orozco <jean.orozco@uees.edu.ec>
2026-09-20 21:05:26 -0500

    refactor: extraer calculo de total

--- src/main/java/edu/uees/refactor/service/ServicioReservas.java
+++ src/main/java/edu/uees/refactor/service/ServicioReservas.java
@@ -34,11 +34,7 @@ public class ServicioReservas {
             return 0;
         }
 
-        double total = 40;
-
-        if ("VIP".equals(r.getTipo())) {
-            total = total * 0.85;
-        }
+        double total = calcularTotal(r);
 
         System.out.println(
                 "Guardando reserva " + r.getId()
@@ -52,4 +48,13 @@ public class ServicioReservas {
 
         return total;
     }
+    private double calcularTotal(Reserva r) {
+    double total = 40;
+
+    if ("VIP".equals(r.getTipo())) {
+        total = total * 0.85;
+    }
+
+    return total;
+}
 }
